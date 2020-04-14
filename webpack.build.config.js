5a9d529f2830da0a865970bf278d91913f47c527
Karel Blavka <karel.blavka@hardwario.com>
2020-04-14 15:15:39 +0200

    Update webpack config
    fix file-loader options

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -12,59 +12,67 @@ const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const defaultInclude = [SRC_DIR];
 
 module.exports = {
-  entry: SRC_DIR + "/index.js",
-  output: {
-    path: OUTPUT_DIR,
-    publicPath: "./",
-    filename: "bundle.js"
-  },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
+    entry: SRC_DIR + "/index.js",
+    output: {
+        path: OUTPUT_DIR,
+        publicPath: "./",
+        filename: "bundle.js"
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }),
+                include: [/node_modules/, defaultInclude]
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                include: defaultInclude
+            },
+            {
+                test: /\.jsx?$/,
+                use: ["babel-loader", "shebang-loader"]
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                use: [{
+                    loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]", options: {
+                        esModule: false,
+                    },
+                }],
+                include: defaultInclude
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [{
+                    loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]", options: {
+                        esModule: false,
+                    },
+                }],
+                include: [/node_modules/, SRC_DIR]
+            }
+        ]
+    },
+    target: "electron-renderer",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "HARDWARIO Playground v" + process.env.npm_package_version
         }),
-        include: [/node_modules/, defaultInclude]
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        include: defaultInclude
-      },
-      {
-        test: /\.jsx?$/,
-        use: ["babel-loader", "shebang-loader"]
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [{ loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]" }],
-        include: defaultInclude
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [{ loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]" }],
-        include: [/node_modules/, SRC_DIR]
-      }
-    ]
-  },
-  target: "electron-renderer",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "HARDWARIO Playground v" + process.env.npm_package_version
-    }),
-    new ExtractTextPlugin("bundle.css"),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-    }),
-    new BabiliPlugin()
-  ],
-  stats: {
-    colors: true,
-    children: false,
-    chunks: false,
-    modules: false
-  }
+        new ExtractTextPlugin("bundle.css"),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify("production"),
+        }),
+        new BabiliPlugin()
+    ],
+    stats: {
+        colors: true,
+        children: false,
+        chunks: false,
+        modules: false
+    }
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,66 +11,75 @@ const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const defaultInclude = [SRC_DIR];
 
 module.exports = {
-  entry: SRC_DIR + "/index.js",
-  output: {
-    path: OUTPUT_DIR,
-    publicPath: "/",
-    filename: "bundle.js"
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-        include: [defaultInclude, /node_modules/]
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        include: defaultInclude
-      },
-      {
-        test: /\.jsx?$/,
-        use: ["babel-loader", "shebang-loader"]
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [{ loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]" }],
-        include: defaultInclude
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [{ loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]" }],
-        include: [/node_modules/, SRC_DIR]
-      }
-    ]
-  },
-  target: "electron-renderer",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "HARDWARIO Playground v" + process.env.npm_package_version
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development")
-    })
-  ],
-  devtool: "cheap-source-map",
-  devServer: {
-    contentBase: OUTPUT_DIR,
-    stats: {
-      colors: true,
-      chunks: false,
-      children: false
+    entry: SRC_DIR + "/index.js",
+    output: {
+        path: OUTPUT_DIR,
+        publicPath: "/",
+        filename: "bundle.js"
     },
-    after() {
-      spawn(
-        "electron",
-        ["."],
-        { shell: true, env: process.env, stdio: "inherit" }
-      )
-        .on("close", code => process.exit(0))
-        .on("error", spawnError => console.error(spawnError));
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                include: [defaultInclude, /node_modules/]
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                include: defaultInclude
+            },
+            {
+                test: /\.jsx?$/,
+                use: ["babel-loader", "shebang-loader"]
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                use: [{
+                    loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]",
+                    options: {
+                        esModule: false,
+                    },
+                }],
+                include: defaultInclude,
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [{
+                    loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]", options: {
+                        esModule: false,
+                    },
+                }],
+                include: [/node_modules/, SRC_DIR]
+            }
+        ]
+    },
+    target: "electron-renderer",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "HARDWARIO Playground v" + process.env.npm_package_version
+        }),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify("development"),
+        })
+    ],
+    devtool: "cheap-source-map",
+    devServer: {
+        contentBase: OUTPUT_DIR,
+        stats: {
+            colors: true,
+            chunks: false,
+            children: false
+        },
+        after() {
+            spawn(
+                "electron",
+                ["."],
+                { shell: true, env: process.env, stdio: "inherit" }
+            )
+                .on("close", code => process.exit(0))
+                .on("error", spawnError => console.error(spawnError));
+        }
     }
-  }
 };
