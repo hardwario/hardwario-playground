f5757a52d10eaff12bc84a6b8a066a3f31483118
Karel Blavka <karel.blavka@hardwario.com>
2020-08-25 14:53:58 +0200

    Update Settings background

--- src/background/Settings.js
+++ src/background/Settings.js
@@ -3,17 +3,19 @@ const ConfigStore = require("../utils/ConfigStore");
 
 var settings = new ConfigStore("settings.json", {
     "language": "en",
-    "mqtt.ip": "127.0.0.1"
+    "mqtt.ip": "127.0.0.1",
+    "enmon-enable": false,
+    "enmon-delay": 5,
 });
 
 function setup() {
     ipcMain.on("settings/get", (event, key) => {
         console.log("settings/get", key, settings.get(key));
-        event.sender.send("settings/value/" + key , settings.get(key) )
+        event.sender.send("settings/value/" + key , settings.get(key) );
     });
 
     ipcMain.on("settings/getAll", (event, dummy) => {
-        event.sender.send("settings/all" , settings.getAll() )
+        event.sender.send("settings/all" , settings.getAll() );
     });
 
     ipcMain.on("settings/set", (event, data) => {
