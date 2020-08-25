ed4099b2b5029cdce7baf113744d23163cab8fe6
Karel Blavka <karel.blavka@hardwario.com>
2020-08-25 14:54:10 +0200

    Update enmon background

--- src/background/enmon.js
+++ src/background/enmon.js
@@ -1,26 +1,62 @@
 const fs = require("fs");
 const path = require("path");
+const { ipcMain } = require("electron");
+const notifyAll = require("../utils/notifyAll");
 const EventEmitter = require('events');
 const { spawn } = require('child_process');
 const mqtt = require("mqtt");
 const { settings } = require('./Settings')
 
-var client;
-var enmon;
-
 class Enmon extends EventEmitter {
 
-    start(delay) {
+    constructor() {
+        super();
+        this._process = null;
+        this._enable = false;
+        this._timeout = false;
+        this._isConnected = false;
+        this._delay = 5;
+    }
+
+    _start() {
+        console.log("Enmon:start", this._enable, this._process === null);
+        if (!this._enable) return;
+        if (this._process) return;
+        if (this._timeout) {
+            clearTimeout(this._timeout);
+        }
+
+        this._isConnected = false;
+
         const programPath = path.join(__dirname, "..", "assets", "enmon");
 
-        this._process = spawn(programPath, ['--loop', '--delay', delay.toString()]);
+        this._process = spawn(programPath, ['--loop', '--delay', this._delay.toString()]);
 
         this.emit('state', 'start');
 
         const self = this;
+
+        this._process.on('close', (code) => {
+            self._process = null;
+            console.log(`Enmon child process exited with code ${code}`);
+
+            if (this._isConnected) {
+                this._isConnected = false;
+                self.emit('state', self.getState());
+            }
+
+            if (self._enable) {
+                self._timeout = setTimeout(() => { self._start(); }, self._delay * 1000);
+            }
+        });
+
         self._line = '';
 
         this._process.stdout.on('data', (buffer) => {
+            if (!this._isConnected) {
+                this._isConnected = true;
+                self.emit('state', self.getState());
+            }
             // console.log(`Enmon stdout: ${buffer}`);
 
             const decoder = new TextDecoder('utf-8');
@@ -41,41 +77,89 @@ class Enmon extends EventEmitter {
             console.error(`Enmon stderr: ${buffer}`);
         });
 
-        this._process.on('close', (code) => {
-            this._process = null;
-            console.log(`Enmon child process exited with code ${code}`);
-            this.emit('state', 'exit');
-        });
     }
 
-    stop() {
+    _stop() {
         if (this._process) {
             this._process.kill();
         }
     }
-}
 
-function start() {
-    if (enmon) return;
-
-    if (!client) {
-        client = mqtt.connect("mqtt://" + settings.get('mqtt.ip'));
+    enable() {
+        if (this._enable) return;
+        this._enable = true;
+        this.emit('state', this.getState());
+        this._start();
     }
 
-    enmon = new Enmon();
-    enmon.start(1);
-    enmon.on('line', (line) => {
-        console.log('Enmon line:', line);
-        if (line.startsWith('@SENSOR: "')) {
-            line = line.substring(10);
-            const i = line.indexOf('",');
-            const name = line.substring(0, i).toLowerCase();
-            const value = line.substring(i + 2);
-            client.publish(`node/enmon/${name}`, value);
+    disable() {
+        if (!this._enable) return;
+        this._enable = false;
+        this._stop();
+        if (this._timeout) {
+            clearTimeout(this._timeout);
         }
-    });
+    }
+
+    getState() {
+        if (!this._enable) return 'disabled';
+        return this._isConnected ? 'online' : 'offline';
+    }
+}
+
+var client;
+var enmon;
+
+function enable(value) {
+
+    if (value) {
+        if (enmon) {
+            enmon.enable();
+
+        } else {
+            enmon = new Enmon();
+
+            client = mqtt.connect("mqtt://" + settings.get('mqtt.ip'));
+
+            enmon.on('line', (line) => {
+                console.log('Enmon line:', line);
+                if (line.startsWith('@SENSOR: "')) {
+                    line = line.substring(10);
+                    const i = line.indexOf('",');
+                    const name = line.substring(0, i).toLowerCase();
+                    const value = line.substring(i + 2);
+                    client.publish(`bridge/${name}`, value);
+                }
+            });
+
+            enmon.on('state', (state) => {
+                console.log('Enmon state', state);
+                notifyAll('bridge/status', { status: state });
+            });
+
+            enmon.enable();
+        }
+    } else {
+        if (enmon) {
+            enmon.disable()
+        }
+    }
 }
 
 module.exports.setup = () => {
-    // start(1);
+
+    settings.on('enmon-enable', (value) => {
+        console.log('enmon-enable', value);
+        enable(value);
+    });
+
+    settings.on('enmon-delay', (value) => {
+        console.log('enmon-delay', value);
+    });
+
+    enable(settings.get('enmon-enable'));
+
+    ipcMain.on("bridge/status/get", (event, data) => {
+        event.sender.send("bridge/status", { status: !enmon ? 'disabled' : enmon.getState() } );
+    });
 };
