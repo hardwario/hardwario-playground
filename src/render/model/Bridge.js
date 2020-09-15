74bf2d52fec88badd7faaaad32a14f1315411cf9
Karel Blavka <karel.blavka@hardwario.com>
2020-09-15 15:12:50 +0200

    Update Bridge model, switch from mqtt to ipc

--- src/render/model/Bridge.js
+++ src/render/model/Bridge.js
@@ -1,5 +1,6 @@
 const EventEmitter = require('events');
-const mqtt = require("mqtt");
+// const mqtt = require("mqtt");
+const { ipcRenderer } = require("electron");
 
 export default class extends EventEmitter {
 
@@ -9,8 +10,75 @@ export default class extends EventEmitter {
         this._connect = false;
         this._values_map = {};
         this._values_list = [];
+
+        ipcRenderer.on("bridge/msg", this.ipcBridgeMsq.bind(this));
+        ipcRenderer.on("bridge/status", this.ipcBridgeStatus.bind(this));
+
+        this._newMessage = this._newMessage.bind(this);
+
+        this._status = "disabled";
+        this._showUdevHint = false;
     }
 
+    ipcBridgeMsq(sender, message) {
+        // console.log('ipcBridgeMsq', message);
+        this._newMessage(message);
+    }
+
+    _newMessage(message) {
+        // console.log('_newMessage', message);
+
+        const tmp = this._values_map[message.label];
+        if (tmp) {
+            tmp.value = message.value;
+            tmp.time = message.time || new Date();
+        } else {
+            this._values_map[message.label] = {
+                label: message.label,
+                value: message.value,
+                time: message.time || new Date()
+            };
+            this._values_list = Object.values(this._values_map).sort((a,b)=>{return a.label < b.label ? -1 : 1});
+        }
+
+        this.emit("message", message);
+    }
+
+    ipcBridgeStatus(sender, payload) {
+        if (this._status !== payload.status || this._showUdevHint !== payload.showUdevHint) {
+            this._status = payload.status;
+            this._showUdevHint = payload.showUdevHint;
+
+            if (payload.status !== 'online') {
+                this.resetValuesList();
+            }
+
+            this.emit("status", payload);
+        }
+    }
+
+    getValues() {
+        return this._values_list;
+    }
+
+    resetValuesList() {
+        this._values_map = {};
+        this._values_list = [];
+    }
+
+    getStatus() {
+        return this._status;
+    }
+
+    getShowUdevHint() {
+        return this._showUdevHint;
+    }
+
+    isConnect() {
+        return true; // this._connect;
+    }
+
+    /* Disable mqtt used ipc
     connect(url) {
         if (this._url == url) return;
 
@@ -53,17 +121,5 @@ export default class extends EventEmitter {
             this.emit("message", message);
         });
     }
-
-    isConnect() {
-        return this._connect;
-    }
-
-    getValues() {
-        return this._values_list;
-    }
-
-    resetValuesList() {
-        this._values_map = {};
-        this._values_list = [];
-    }
+    */
 }
