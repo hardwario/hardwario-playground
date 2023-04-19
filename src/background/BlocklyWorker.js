4e3437b273e752791e3660e49fb00f20565be89a
Jakub Smejkal <smejkal.jakub99@gmail.com>
2023-04-19 21:17:55 +0200

    Remove code that caused startup error

--- src/background/BlocklyWorker.js
+++ src/background/BlocklyWorker.js
@@ -1,8 +1,6 @@
 const blockly = require("hardwario-blockly-dev");
-const shell = require('shelljs');
 
 function setup() {
-    shell.config.execPath = shell.which('node').toString()
     blockly.init();
 }
 
