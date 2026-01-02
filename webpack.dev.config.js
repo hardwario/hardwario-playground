d1662bd1f26a04364d065bf76527663b662e7216
David Kopl - PC <kopldavid@seznam.cz>
2026-01-02 09:11:23 +0100

    upgrade sass to fix warnings

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -27,7 +27,18 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                silenceDeprecations: ["legacy-js-api", "import"]
+                            }
+                        }
+                    }
+                ],
                 include: defaultInclude
             },
             {
